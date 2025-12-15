88feb8b251bf77b337a6d3ece6daad3b1a524695
aegistech <vietnamcolen2021b@gmail.com>
2025-12-15 22:39:07 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,6 +35,8 @@ export default {
       animation: {
         'snow': 'snow 10s linear infinite',
         'marquee': 'marquee 30s linear infinite',
+        'blob': 'blob 7s infinite',
+        'spin-slow': 'spin 3s linear infinite',
       },
       keyframes: {
         snow: {
@@ -45,7 +47,21 @@ export default {
         marquee: {
           '0%': { transform: 'translateX(0%)' },
           '100%': { transform: 'translateX(-100%)' },
-        }
+        },
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+        },
       }
     },
   },
